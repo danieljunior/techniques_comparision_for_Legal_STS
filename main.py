940ecb25e1972ec994fbf6de0e0297ec7927dc90
Daniel Junior <danieljunior@id.uff.br>
2021-01-29 13:19:07 -0300

    V1 for experiments

--- main.py
+++ main.py
@@ -20,80 +20,88 @@ def setup_indexer(vectors_size=3072):
     return AnnoyIndex(vectors_size, 'angular')
 
 logger.info('Loading data...')
-X_train, X_test, y_train, y_test = prepare_data()
+train, test = prepare_data(test_size=0.5)
 
 logger.info('Loading models...')
 
-# tfidf = TfIdfEmbedder(X_train[:10])
-# tfidf.set_indexer(setup_indexer(vectors_size=len(tfidf.get_feature_names())))
-# dictionary = dict(zip(tfidf.get_feature_names(),
-#                       list(tfidf.idf_)))
+tfidf = TfIdfEmbedder(X_train[:10])
+tfidf.set_indexer(setup_indexer(vectors_size=len(tfidf.get_feature_names())))
+dictionary = dict(zip(tfidf.get_feature_names(),
+                      list(tfidf.idf_)))
 
 
-# word2vec = Word2VecEmbedder('models/word2vec/skip_s300.txt', 
-#                             indexer = setup_indexer(vectors_size=300))
+word2vec = Word2VecEmbedder('models/word2vec/skip_s300.txt', 
+                            indexer = setup_indexer(vectors_size=300))
 
-# weighted_word2vec = Word2VecEmbedder('models/word2vec/skip_s300.txt', 
-#                             tfidf_model=tfidf, 
-#                             tfidf_dictionary=dictionary,
-#                             indexer = setup_indexer(vectors_size=300))
+weighted_word2vec = Word2VecEmbedder('models/word2vec/skip_s300.txt', 
+                            tfidf_model=tfidf, 
+                            tfidf_dictionary=dictionary,
+                            indexer = setup_indexer(vectors_size=300))
 
-# fasttext = Word2VecEmbedder('models/fasttext/skip_s300.txt', 
-#                             indexer = setup_indexer(vectors_size=300))
+fasttext = Word2VecEmbedder('models/fasttext/skip_s300.txt', 
+                            indexer = setup_indexer(vectors_size=300))
 
-# weighted_fasttext = Word2VecEmbedder('models/fasttext/skip_s300.txt', 
-#                             tfidf_model=tfidf, 
-#                             tfidf_dictionary=dictionary,
-#                             indexer = setup_indexer(vectors_size=300))
+weighted_fasttext = Word2VecEmbedder('models/fasttext/skip_s300.txt', 
+                            tfidf_model=tfidf, 
+                            tfidf_dictionary=dictionary,
+                            indexer = setup_indexer(vectors_size=300))
 
 sentence_transformer = SentenceTransformerEmbedder('distiluse-base-multilingual-cased-v2', 
                                                    setup_indexer(vectors_size=512))
-# options_path = 'models/elmo/options.json'
-# weights_path = 'models/elmo/elmo_pt_weights_dgx1.hdf5'
-# elmo = ElmoEmbedder(options_path, weights_path, setup_indexer(vectors_size=1024))
+options_path = 'models/elmo/options.json'
+weights_path = 'models/elmo/elmo_pt_weights_dgx1.hdf5'
+elmo = ElmoEmbedder(options_path, weights_path, setup_indexer(vectors_size=1024))
 
-# bert = BertEmbedder('models/bert-base-cased-pt-br', setup_indexer())
-# longformer = LongformerEmbedder('models/bert_longformer', setup_indexer())
-# itd_longformer = LongformerEmbedder('models/itd_bert_longformer', setup_indexer())
+bert = BertEmbedder('models/bert-base-cased-pt-br', setup_indexer())
 
-# TODO falta o finetunning_bert, bm25
+itd_bert = BertEmbedder('models/itd_bert', setup_indexer())
+
+longformer = LongformerEmbedder('models/bert_longformer', setup_indexer())
+
+itd_longformer = LongformerEmbedder('models/itd_bert_longformer', setup_indexer())
+
+# TODO falta o bm25
 
 embedders = {
-    # 'tfidf': tfidf,
-    # 'word2vec': word2vec,
-    # 'weighted_word2vec': weighted_word2vec,    
-    # 'fasttext': fasttext,
-    # 'weighted_fasttext': weighted_fasttext,
-    'sentence_transformer': sentence_transformer
-    # 'elmo': elmo, 
-    # 'bert':bert,
-    # 'longformer':longformer,
-    # 'itd_longformer': itd_longformer
+    'tfidf': tfidf,
+    'word2vec': word2vec,
+    'weighted_word2vec': weighted_word2vec,    
+    'fasttext': fasttext,
+    'weighted_fasttext': weighted_fasttext,
+    'sentence_transformer': sentence_transformer,
+    'elmo': elmo, 
+    'bert':bert,
+    'itd_bert': itd_bert,
+    'longformer':longformer,
+    'itd_longformer': itd_longformer
 }
 
-for k,v in tqdm(embedders.items()):
-    model = v
-    for i, ementa in tqdm(enumerate(X_train[:10])):
-        # import pdb; pdb.set_trace()
-        embeddings = model.get_embeddings(ementa)[0]
-        model.add_to_indexer(i, embeddings)
+results = []
+for model_name, model in tqdm(embedders.items()):
 
-    model.save_indexer('results/'+k+'.ann')
+    logger.info(model_name.upper())
+    logger.info('Getting embeddings and add to indexer...')
+    for index, doc in tqdm(train.iterrows()):
+        embeddings = model.get_embeddings(doc.ementa)[0]
+        model.add_to_indexer(index, embeddings)
 
-# index = 0
-# indexer.add_item(index, embeddings)
+    model.save_indexer('results/'+model_name+'.ann')
+    
+    logger.info('Getting neighbors...')
+    for source_index, doc in tqdm(train.iterrows()):
+        nns = model.indexer.get_nns_by_item(source_index, 6)
+        source_vector = model.indexer.get_item_vector(source_index)
+        
+        for similar_index in nns[1:]:
+            similar_vector = model.indexer.get_item_vector(similar_index)
+            similarity = cosine_similarity([source_vector], [similar_vector])[0][0]
+            results.append([source_index, similar_index, similarity, model_name])    
 
-# indexer.build(10) # 10 trees
-# indexer.save('results/bertlongformer.ann')
-
+logger.info('Saving results...')
+data = pd.DataFrame(results, 
+                    columns=['SOURCE_INDEX','SIMILAR_INDEX','COSINE_SIMILARITY','MODEL_NAME'])
+data.to_csv('results/similarities.csv')
 # indexer = setup_indexer()
 # indexer.load('results/bertlongformer.ann')
 
-# nns = indexer.get_nns_by_item(index, 5)
-# a = indexer.get_item_vector(index)
-
-# for n in nns[1:]:
-#         b = indexer.get_item_vector(n)
-#         similaridade = cosine_similarity([a], [b])[0][0]
-#         print(similaridade)
 logger.info('Finish...')
--- utils.py
+++ utils.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def prepare_data():
+def prepare_data(test_size=0.2):
     data = pd.read_csv('datasets/jurisprudencias_stj.csv', index_col=0)
     v = data[['jurisprudencia_index']]
 
     # only data from jurisprudencies with more than 1 
     custom_data = data[v.replace(v.apply(pd.Series.value_counts)).gt(2).all(1)]
-    X_train, X_test, y_train, y_test = train_test_split(custom_data.ementa, 
-                                                        custom_data.jurisprudencia_index, 
-                                                        test_size=0.2, 
-                                                        stratify=custom_data.jurisprudencia_index, 
-                                                        random_state=42)
-    return X_train, X_test, y_train, y_test
+    custom_data.to_csv('datasets/custom_data.csv')
+    train, test = train_test_split(custom_data, 
+                            test_size=test_size, 
+                            stratify=custom_data.jurisprudencia_index,
+                            shuffle=True,
+                            random_state=42)
+    return train, test
