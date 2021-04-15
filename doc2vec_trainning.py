1b1cca7e810ac5cb643070ec66a763d8a2b17590
Daniel Junior <danieljunior@id.uff.br>
2021-04-15 18:00:50 -0300

    Alterando treinamento do doc2vec

--- doc2vec_trainning.py
+++ doc2vec_trainning.py
@@ -2,23 +2,42 @@ import pandas as pd
 import gensim
 from os import listdir
 from tqdm import tqdm 
+            
+class ITDIterator():
+    
+    HOME = 'datasets/itd_corpus'
+    
+    def __init__(self):
+        self.generator_function = self.read_corpus
+        self.generator = self.generator_function()
+    
+    def read_corpus(self, tokens_only=False):
+        for i, f in tqdm(enumerate(listdir(ITDIterator.HOME))):
+            doc = open(ITDIterator.HOME+'/'+f,'r').read()
+            tokens = gensim.utils.simple_preprocess(doc)
 
-HOME = 'datasets/itd_corpus'
-def read_corpus(tokens_only=False):
-    for i, f in tqdm(enumerate(listdir(HOME))):
-        doc = open(HOME+'/'+f,'r').read()
-        tokens = gensim.utils.simple_preprocess(doc)
-        
-        if tokens_only:
-            yield tokens
+            if tokens_only:
+                yield tokens
+            else:
+                # For training data, add tags
+                yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
+
+    def __iter__(self):
+        # reset the generator
+        self.generator = self.generator_function()
+        return self
+
+    def __next__(self):
+        result = next(self.generator)
+        if result is None:
+            raise StopIteration
         else:
-            # For training data, add tags
-            yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
+            return result
 
 
 # train_corpus = list(read_corpus())
 model = gensim.models.doc2vec.Doc2Vec(vector_size=100, min_count=5, epochs=10)
-model.build_vocab(read_corpus())
-model.train(read_corpus(), total_examples=model.corpus_count, epochs=model.epochs)
+model.build_vocab(ITDIterator())
+model.train(ITDIterator(), total_examples=model.corpus_count, epochs=model.epochs)
 model.save('models/itd_doc2vec_model')
 # model = gensim.models.doc2vec.Doc2Vec.load('models/itd_doc2vec_model')
